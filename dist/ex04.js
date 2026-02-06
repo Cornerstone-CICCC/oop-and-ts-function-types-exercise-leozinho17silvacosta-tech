1d0fddd0324acda181fcb599e3b67f9d6360a4e7
Leo <leozinho17silvacosta@gmail.com>
2026-02-05 21:03:24 -0800

    Complete TypeScript Types Exercise OOP and TS

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,12 +1,11 @@
+"use strict";
 // Create a function called greet.
 // This function should accept a required name (string) and an optional greeting message (string).
 // If no greeting is provided, use "Hello" as the default message.
 // The function should log the greeting message followed by the name. Specify the correct return type for logging.
-
-function greet(name: string, message?: string): void {
-    const greeting = message ?? "Hello";
+function greet(name, message) {
+    const greeting = message !== null && message !== void 0 ? message : "Hello";
     console.log(`${greeting}, ${name}!`);
 }
-
-greet("John");               // Expected output: Hello, John!
+greet("John"); // Expected output: Hello, John!
 greet("John", "Good morning"); // Expected output: Good morning, John!
